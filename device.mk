5188d4f510e3b19fabed2e877d8349f41ea98088
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    vayu: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- device.mk
+++ device.mk
@@ -389,9 +389,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
@@ -441,6 +439,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # NeuralNetworks
