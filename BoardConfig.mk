e270a7357f7d2ac9d7ce847108eca569fb170eeb
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-09 16:04:34 -0400

    vayu: Simplify AVB flag logic
    * AVB's `--flags 3` argument builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,8 +108,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # Inherit from proprietary files
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
