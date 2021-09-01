a9db43d663f074b144a7ec54c2f1229f1f2a7540
raghavt20 <raghavtandon2000@gmail.com>
2021-09-01 22:50:54 +0200

    vayu: Import config.fs from LA.UM.9.1.r1-11100-SMxxx0.QSSI12.0

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
