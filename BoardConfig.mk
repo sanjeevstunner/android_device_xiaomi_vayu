31f51bd40e48602a25521b58d08794ac7ae37122
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 02:13:25 +0530

    vayu: Define architecture and platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,5 +8,30 @@ BOARD_VENDOR := xiaomi
 
 DEVICE_PATH := device/xiaomi/vayu
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msmnile
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msmnile
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+
 # Inherit from proprietary files
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
