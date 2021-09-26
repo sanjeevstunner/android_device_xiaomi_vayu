e9d1df46214c9d5c2b61a2726e7788b80c2329b0
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 02:18:47 +0530

    vayu: Define kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,30 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 TARGET_BOOTLOADER_BOARD_NAME := msmnile
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    console=ttyMSM0,115200n8 \
+    earlycon=msm_geni_serial,0xa90000 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048 \
+    video=vfb:640x400,bpp=32,memsize=3072000
+
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := vayu_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150
+
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 
