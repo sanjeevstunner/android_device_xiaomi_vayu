ed9d698bd7e7c5273217d7a0826bb652fb09028b
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 01:45:15 +0530

    vayu: Initial device tree makefiles

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR := xiaomi
+
+DEVICE_PATH := device/xiaomi/vayu
---
+++ device.mk
@@ -0,0 +1,6 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
