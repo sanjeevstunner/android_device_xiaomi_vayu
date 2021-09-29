766694d1002f8f6b177f311fb86271ccd963786d
raghavt20 <raghavtandon2000@gmail.com>
2021-09-29 22:22:10 +0530

    vayu: Build qti powerHAL

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
