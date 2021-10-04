0ef3edb8af10b9fc0a2f6ccf3b3a42b077856b51
raghavt20 <raghavtandon2000@gmail.com>
2021-10-04 19:21:24 +0530

    vayu: Build opensource perf 2.2 interface

--- device.mk
+++ device.mk
@@ -232,6 +232,10 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
