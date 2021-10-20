5d664dfd56a64ceef72295df189195b251abefb9
raghavt20 <raghavtandon2000@gmail.com>
2021-10-20 18:06:16 +0530

    vayu: Build libhwbinder vendor varient

--- device.mk
+++ device.mk
@@ -187,6 +187,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
