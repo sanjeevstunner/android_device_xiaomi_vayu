7bf645351d32fc0948e83149147922955b3ae126
raghavt20 <raghavtandon2000@gmail.com>
2021-10-27 01:40:14 +0530

    vayu: Build libkeymaster_messages vendor variant
    - It's required for vayu/bhima's Goodix FP HAL

--- device.mk
+++ device.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.vayu
+    android.hardware.biometrics.fingerprint@2.1-service.vayu \
+    libkeymaster_messages.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
