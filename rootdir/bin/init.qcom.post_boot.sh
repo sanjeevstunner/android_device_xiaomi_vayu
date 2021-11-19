89dbde669695e27072c76c413ba01708071b14fb
raghavt20 <raghavtandon2000@gmail.com>
2021-11-19 13:47:50 +0530

    vayu: Import mem latency specific settings
    - From https://source.codeaurora.org/quic/la/platform/vendor/qcom/msmnile/tree/init.qti.dcvs.sh?h=LA.UM.9.1.r1-11300-SMxxx0.0#n30

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -215,9 +215,41 @@ case "$target" in
         done
     done
 
-    # memlat specific settings are moved to seperate file under
-    # device/target specific folder
-    setprop vendor.dcvs.prop 1
+    for device in /sys/devices/platform/soc
+    do
+        #Enable mem_latency governor for L3, LLCC, and DDR scaling
+        for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
+        do
+            echo "mem_latency" > $memlat/governor
+            echo 10 > $memlat/polling_interval
+            echo 400 > $memlat/mem_latency/ratio_ceil
+            done
+
+        #Enable userspace governor for L3 cdsp nodes
+        for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
+        do
+            echo "cdspl3" > $l3cdsp/governor
+        done
+
+        #Enable compute governor for gold latfloor
+        for latfloor in $device/*cpu-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
+        do
+            echo "compute" > $latfloor/governor
+            echo 10 > $latfloor/polling_interval
+        done
+
+        #Gold L3 ratio ceil
+        for l3gold in $device/*cpu4-cpu-l3-lat/devfreq/*cpu4-cpu-l3-lat
+        do
+            echo 4000 > $l3gold/mem_latency/ratio_ceil
+        done
+
+        #Prime L3 ratio ceil
+        for l3prime in $device/*cpu7-cpu-l3-lat/devfreq/*cpu7-cpu-l3-lat
+        do
+            echo 20000 > $l3prime/mem_latency/ratio_ceil
+        done
+    done;
 
 echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 esac
