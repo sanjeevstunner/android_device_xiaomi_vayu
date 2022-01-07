41d634e185f7d84b2e0f13dbb85208b513df70d0
sanjeevstunner <sanjeevstunner1@gmail.com>
2022-01-07 12:12:32 +0530

    vayu: Drop awaken specific device FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,8 +68,7 @@ TARGET_SCREEN_DENSITY := 440
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
-    vendor/awaken/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 ODM_MANIFEST_FILES += $(DEVICE_PATH)/configs/hidl/manifest-qva.xml
