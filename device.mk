e1071fd16bce389ed0167eb959f4d54aa64fd4d7
sanjeevstunner <sanjeevstunner1@gmail.com>
2022-01-07 10:27:32 +0530

    vayu: Drop Xiaomi Parts

--- device.mk
+++ device.mk
@@ -124,10 +124,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
