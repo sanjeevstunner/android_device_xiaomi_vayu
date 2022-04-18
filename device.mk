3acc51bcd3f48be660db671f74ac7836efb41aa1
sanjeevstunner <sanjeevstunner1@gmail.com>
2022-04-18 11:21:55 +0530

    Revert "vayu: build vendor.xiaomi.hardware.touchfeature@1.0"
    This reverts commit e5ddb51d57a61565ea42d19f0860f23e941d506c.

--- device.mk
+++ device.mk
@@ -382,11 +382,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# Touchfeature
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.touchfeature@1.0 \
-    vendor.xiaomi.hardware.touchfeature@1.0.vendor
-
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
