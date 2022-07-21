16555a034c45a5e4c08dd16e6f3ed39879fd8e97
Sanjeev <sanjeevstunner1@gmail.com>
2022-07-21 12:33:44 +0530

    Migrate from Device Package Overlays to Runtime Resource Overlays
    * FrameworksResOverlay: Move dimens to separate file
    * SystemUIOverlay: Move some dimens to its file
    
    Referred for some stuff from Arrow OS vayu DT
    Ref: https://github.com/ArrowOS-Devices/android_device_xiaomi_vayu/tree/arrow-12.1/rro_overlays

--- device.mk
+++ device.mk
@@ -328,11 +328,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_vayu/com.nxp.mifare.xml
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-   NotchBarKillerVayu
+   NotchBarKillerVayu \
+   BluetoothResOverlay \
+   FrameworksResOverlay \
+   SettingsOverlay \
+   SettingsProviderOverlay \
+   SystemUIOverlay \
+   TelephonyOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
