a247808cfa93457ad0b3fd08e029c78830ff7b98
Sanjeev <sanjeevstunner1@gmail.com>
2022-07-31 19:01:11 +0530

    Revert "Migrate from Device Package Overlays to Runtime Resource Overlays"
    This reverts commit 0a0c4b78463d007fa6313f754eb6323b664fdcc7.

--- device.mk
+++ device.mk
@@ -339,15 +339,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_vayu/com.nxp.mifare.xml
 
 # Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-   NotchBarKillerVayu \
-   BluetoothResOverlay \
-   FrameworksResOverlay \
-   SettingsOverlay \
-   SettingsProviderOverlay \
-   SystemUIOverlay \
-   TelephonyOverlay
+   NotchBarKillerVayu
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
