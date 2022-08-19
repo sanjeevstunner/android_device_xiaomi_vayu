655d09e863a4d6a7b292b32b28456eb4bd1aabb3
tarsin <yuanqingxiang233@163.com>
2022-08-19 11:03:33 +0000

    vayu: Make shim for com.qti.node.watermark.so
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I47fea33432692a7a76d71db91676cb3a58ac19c7

--- device.mk
+++ device.mk
@@ -107,7 +107,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libshim_megvii \
-    libpiex_shim
+    libpiex_shim \
+    libwatermark_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
