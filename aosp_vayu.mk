85768185b812422e9ae50f61329433607c1daacc
Sanjeev <sanjeevstunner1@gmail.com>
2022-09-19 17:48:00 +0530

    Pixel Experience bringup
    *drop Arrow OS stuff while we're at it
    *adapt kernel config

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common configurations from Pixel Experience 
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -25,7 +25,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=vayu_global \
     PRODUCT_MODEL=M2102J20SI
-
-# ArrowOS additions
-DEVICE_MAINTAINER := kubersharma001
-
