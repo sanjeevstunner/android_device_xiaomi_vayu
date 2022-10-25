1b0af4b134d2e980b58b0df7030a9c415539174e
Akash Srivastava <akashniki@gmail.com>
2022-10-25 21:46:58 +0530

    vayu: Build android.hidl.memory.block and android.hidl.memory@1.0-impl
    * Used for dynamic memory management
    
    Change-Id: I7f39d3c346498a527294c284e8577662ac844f9e

--- device.mk
+++ device.mk
@@ -252,6 +252,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     libhwbinder.vendor
 
+PRODUCT_PACKAGES += \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory@1.0-impl
+
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
