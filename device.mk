46bc1124641dbcb3d79fbad2406e857b228dd37c
Georg Veichtlbauer <georg@vware.at>
2023-04-03 12:24:00 +0530

    vayu: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- device.mk
+++ device.mk
@@ -480,8 +480,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
-    NfcNci \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
