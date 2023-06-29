c15f51ed37a0784b9b1f1189ff41e89793067e93
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2023-06-29 21:18:28 +0530

    Revert "vayu: Drop build {description, fingerprint} from MIUI"
    This reverts commit f9401daf9a252cd08c2a01fb6eaf78e62f20f446.

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -27,5 +27,8 @@ CUSTOM_DEVICE := vayu
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vayu_global-user 12 SKQ1.211006.001 V13.0.5.0.SJUMIXM release-keys" \
     PRODUCT_NAME=vayu_global \
     PRODUCT_MODEL=M2102J20SI
+
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
