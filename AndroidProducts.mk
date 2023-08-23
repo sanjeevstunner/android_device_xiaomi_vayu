2984c7b725a74e72c2626c8489d2510a3add4d39
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2023-08-23 10:16:03 +0530

    Bringup Lineage OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_vayu.mk
+    $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_vayu-eng \
-    aosp_vayu-userdebug \
-    aosp_vayu-user
+    lineage_vayu-eng \
+    lineage_vayu-userdebug \
+    lineage_vayu-user
--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -11,13 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common Pixel Experience configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common Lineage configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Feature Flags
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
